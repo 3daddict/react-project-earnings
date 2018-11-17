5619b44e3af86fa4a7648466f9f5c5638fa1a8c2
Michael Salvati <print@3daddict.com>
2018-11-16 16:00:36 -0800

    bootstrap navbar created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/nav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Nav />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
